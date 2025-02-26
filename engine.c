5458185a6cec0756a1a62c7d789ce208bb06a4ed
abdo_fighterCoding <144532259+amroKing@users.noreply.github.com>
2025-02-26 18:12:07 +0000

    Update engine.c

--- engine.c
+++ engine.c
@@ -13,19 +13,5 @@ void    fractal_init(t_fractal *fractal)
 		free(fractal->mlx_connect);
 		malloc_error();
 	}
-	// fractal->img.img_ptr = mlx_new_image(fractal->mlx_connect,
-	// 									WIDTH, HEIGHT);
-	// if (NULL == fractal->img.img_ptr)
-	// {
-	// 	mlx_destroy_window(fractal->mlx_connect, fractal->mlx_window);
-	// 	mlx_destroy_display(fractal->mlx_connect);
-	// 	free(fractal->mlx_connect);
-	// 	malloc_error();
-	// }
-	// fractal->img.pixels_ptr = mlx_get_data_addr(fractal->img.img_ptr,
-	// 											&fractal->img.bpp,
-	// 											&fractal->img.line_len,
-	// 											&fractal->img.endian);
-	// events_init(fractal);
-	// data_init(fractal);
+
 }
